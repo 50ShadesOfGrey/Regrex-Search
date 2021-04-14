e8ef96b2bb294b1af56faf1de765c60a83628df1
patels29 <49618413+patels29@users.noreply.github.com>
2021-04-14 16:58:57 -0400

    Add files via upload

--- wiki/web/forms.py
+++ wiki/web/forms.py
@@ -5,10 +5,13 @@
 from flask_wtf import Form
 from wtforms import BooleanField
 from wtforms import TextField
+from wtforms import DateField
+from wtforms import StringField
 from wtforms import TextAreaField
 from wtforms import PasswordField
 from wtforms.validators import InputRequired
 from wtforms.validators import ValidationError
+
 
 from wiki.core import clean_url
 from wiki.web import current_wiki
@@ -32,6 +35,12 @@ class SearchForm(Form):
         description='Ignore Case',
         # FIXME: default is not correctly populated
         default=True)
+    date_option = DateField(
+        description='Date'
+    )
+    keyword = StringField(
+        description='Keyword'
+    )
 
 
 class EditorForm(Form):
